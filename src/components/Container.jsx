e9daa237c861474df801f9c9f65d671d2bfd1447
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-29 19:52:15 +0200

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import uuid from 'uuid';
-import Friends from './Friends';
+import Friend from './Friend';
 import FriendEditor from './FriendEditor';
 
+const initialFormState = {
+  nameValue: '',
+  ageValue: '',
+};
 
 const initialState = {
   friends: [
@@ -11,57 +15,67 @@ const initialState = {
     { id: uuid(), name: 'Josh', age: '34' },
   ],
   currentFriendId: null,
+  form: initialFormState,
 };
 
 class Container extends React.Component {
-  state = initialState
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
 
   addFriend = (name, age) => {
-    // add a new friend object to `state.friends` using setState
+    // using setState:
+    // 1- add a new friend object to state.friends
+    // 2- reset the form to its initial state
   }
 
   updateFriend = (id, name, age) => {
-    // update an existing friend (the `id` tells us which friend to update)
-    // using setState
+    // using setState:
+    // 1- update an existing friend (the `id` tells us which friend to update)
+    // 2- also reset currentFriendId to null
+    // 3- reset the form to its initial state
   }
 
   deleteFriend = id => {
-    // 1- delete an existing friend (the `id` tells us which) using setState
+    // using setState:
+    // 1- delete an existing friend (the `id` tells us which)
     // 2- also set currentFriendId to null
+    // 3- reset the form to its initial state
   }
 
   setCurrentFriendId = id => {
     // set state.currentFriendId to be `id`
   }
 
-  render() {
-    const currentFriend = this.state.friends.find(
-      Function.prototype,
-      // find the friend object that has an id of `state.currentFriendId`
-    );
+  inputChange = (value, field) => {
+    // implement with setState
+  }
 
+  render() {
     return (
       <div className='container'>
-        <Friends
-          friends={this.state.friends}
-          deleteFriend={this.deleteFriend}
-          setCurrentFriendId={this.setCurrentFriendId}
+        <div className='sub-container'>
+          <h3>These are my friends:</h3>
+          {/* Make it so we get the `No friends! Sad!` h5 if there are no friends */}
+          {
+            this.state.friends.map(friend => (
+              <Friend
+                key={friend.id}
+                friend={friend}
+                deleteFriend={this.deleteFriend}
+                setCurrentFriendId={this.setCurrentFriendId}
+              />
+            ))
+          }
+        </div>
+        <FriendEditor
+          form={this.state.form}
+          inputChange={this.inputChange}
+          addFriend={this.addFriend}
+          updateFriend={this.updateFriend}
+          isEditing={!!this.state.currentFriendId}
         />
-
-        {
-          !this.state.currentFriendId &&
-          <FriendEditor
-            addFriend={this.addFriend}
-          />
-        }
-        {
-          this.state.currentFriendId &&
-          <FriendEditor
-            currentFriend={currentFriend}
-            updateFriend={this.updateFriend}
-            setCurrentFriendId={this.setCurrentFriendId}
-          />
-        }
       </div>
     );
   }
--- src/components/FriendEditor.jsx
+++ src/components/FriendEditor.jsx
@@ -1,59 +1,20 @@
 import React from 'react';
 
 
-const initialState = {
-  nameValue: '',
-  ageValue: '',
-};
-
 export class FriendEditor extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = props.currentFriend
-      ? {
-        nameValue: props.currentFriend.name,
-        ageValue: props.currentFriend.age,
-      }
-      : initialState;
-  }
-
-  clearInputs = () => {
-    // implement with setState
-  }
-
   onNameChange = event => {
-    // implement with setState
+    // you'll need this.props.inputChange
   }
 
   onAgeChange = event => {
-    if (Number(event.target.value)) {
-      // implement with setState
-    }
-  }
-
-  onFriendAdd = () => {
-    if (this.state.nameValue && this.state.ageValue) {
-      // 1- add friend using the `addFriend` function coming in via props
-      // 2- clear the inputs
-    }
-  }
-
-  onFriendUpdate = () => {
-    if (this.state.nameValue && this.state.ageValue) {
-      // 1- update friend using the `updateFriend` function coming in via props
-      // 2- set the current friend id to null using function coming in via props
-      // 3- clear the inputs
-    }
+    // you'll need this.props.inputChange
   }
 
   render() {
-    const isEditing = this.props.currentFriend;
-
     return (
       <div className='sub-container'>
         {
-          isEditing
+          this.props.isEditing
             ? <h3>Edit Friend</h3>
             : <h3>Add a friend!</h3>
         }
@@ -61,19 +22,19 @@ export class FriendEditor extends React.Component {
         name:
         <input
           type="text"
-          value={this.state.nameValue}
+          value={this.props.form.nameValue}
           onChange={this.onNameChange}
         />
 
         age:
         <input
           type="text"
-          value={this.state.ageValue}
+          value={this.props.form.ageValue}
           onChange={this.onAgeChange}
         />
 
         {
-          isEditing
+          this.props.isEditing
             ? <button onClick={this.onFriendUpdate}>Update Friend!</button>
             : <button onClick={this.onFriendAdd}>Add Friend!</button>
         }
