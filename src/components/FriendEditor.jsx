b249934e3ccec1efee01810fcee3c1cd27cdb245
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-29 20:22:00 +0200

    wip

--- src/components/FriendEditor.jsx
+++ src/components/FriendEditor.jsx
@@ -1,44 +1,48 @@
 import React from 'react';
 
 
-export default class FriendEditor extends React.Component {
-  onNameChange = event => {
-    // you'll need this.props.inputChange
-  }
+export default function FriendEditor({
+  form,
+  onFriendAdd,
+  onFriendUpdate,
+  isEditing,
+  inputChange,
+}) {
+  const onNameChange = event => {
+    // you'll need inputChange
+  };
 
-  onAgeChange = event => {
-    // you'll need this.props.inputChange
-  }
+  const onAgeChange = event => {
+    // you'll need inputChange
+  };
 
-  render() {
-    return (
-      <div className='sub-container'>
-        {
-          this.props.isEditing
-            ? <h3>Edit Friend</h3>
-            : <h3>Add a new friend!</h3>
-        }
+  return (
+    <div className='sub-container'>
+      {
+        isEditing
+          ? <h3>Edit Friend</h3>
+          : <h3>Add a new friend!</h3>
+      }
 
-        name:
-        <input
-          type="text"
-          value={this.props.form.nameValue}
-          onChange={this.onNameChange}
-        />
+      name:
+      <input
+        type="text"
+        value={form.nameValue}
+        onChange={onNameChange}
+      />
 
-        age:
-        <input
-          type="text"
-          value={this.props.form.ageValue}
-          onChange={this.onAgeChange}
-        />
+      age:
+      <input
+        type="text"
+        value={form.ageValue}
+        onChange={onAgeChange}
+      />
 
-        {
-          this.props.isEditing
-            ? <button onClick={this.onFriendUpdate}>Update Friend!</button>
-            : <button onClick={this.onFriendAdd}>Add Friend!</button>
-        }
-      </div>
-    );
-  }
+      {
+        isEditing
+          ? <button onClick={onFriendUpdate}>Update Friend!</button>
+          : <button onClick={onFriendAdd}>Add Friend!</button>
+      }
+    </div>
+  );
 }
